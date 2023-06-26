477b9c309e47e6ca8236d6ec99dca72849b37b25
Viktor Benei <viktorbenei@bitrise.io>
2023-06-26 20:06:15 +0200

    Initial Commit

---
+++ Bitrise TODOs Sample/Bitrise_TODOs_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_TODOs_SampleApp.swift
+//  Bitrise TODOs Sample
+//
+//  Created by viktorbenei on 2023. 06. 26..
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_TODOs_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
